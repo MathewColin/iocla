9a2a60bb5fdd4f090904c8aca533ea7ff25f499b
Ceausu Matei Calin <mateicalin2002@yahoo.com>
2022-03-05 04:45:48 +0200

    Laborator 1, Add solution for exercise 3:
    For this exercise I used the function created at the previous one (my_strlen)
    and just swapped the i-th character with the i-th character counting from
    the end.
    
    Signed-off-by: Ceausu Matei Calin <mateicalin2002@yahoo.com>

--- laborator/content/reprezentare-numere/3-mirror/mirror.c
+++ laborator/content/reprezentare-numere/3-mirror/mirror.c
@@ -2,16 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 
+int my_strlen(const char *str)
+{
+	/* TODO */
+	int adv = 1;
+	int i = 0;
+	while (adv) {
+		if (!(*(str + i)))
+			return i;
+		i++;
+	}
+	return -1;
+}
+
 void mirror(char *s)
 {
 	/* TODO */
-	(void) s;
+	int i = 0;
+	int l = my_strlen(s) / 2;
+	int L = my_strlen(s);
+	while (i < l) {
+		char c = *(s + i);
+		*(s + i) = *(s + L - i - 1);
+		*(s + L - i - 1) = c;
+		i++;
+	}
+	return;
 }
 
 int main()
 {
 	/* TODO: Test function */
-
+	char s[101];
+	scanf("%s", s);
+	mirror(s);
+	printf("%s\n", s);
 	return 0;
 }
 
