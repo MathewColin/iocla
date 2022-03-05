b57c57c3029c409db5182b01f4b62dbeba66e36e
Ceausu Matei Calin <mateicalin2002@yahoo.com>
2022-03-05 03:07:18 +0200

    Laborator 1, Add solution for exercise 2:
    For my_strlen function, I went through the elements of the array until I found
    the null character and count all the characters before the null character.
    
    For equality_check, I went through the elements of the array and searched for
    the i-th element that is equal to (i + 2^i)%length element and then I print
    the address.
    
    Signed-off-by: Ceausu Matei Calin <mateicalin2002@yahoo.com>

--- laborator/content/reprezentare-numere/2-len_xor/len_xor.c
+++ laborator/content/reprezentare-numere/2-len_xor/len_xor.c
@@ -2,24 +2,52 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define max_length 25
+
 int my_strlen(const char *str)
 {
 	/* TODO */
-	(void) str;
-
+	int adv = 1;
+	/*for (int i = 0; adv; i++) {
+		if (!(*(str + i)))
+			return i;
+	}*/
+	int i = 0;
+	while (adv) {
+		if (!(*(str + i)))
+			return i;
+		i++;
+	}
 	return -1;
 }
 
 void equality_check(const char *str)
 {
 	/* TODO */
-	(void) str;
+	int i = 0, l;
+	l = my_strlen(str);
+	while (i < l) {
+		int x = i + (1 << i);
+		if (x >= l)
+			x %= l;
+		if (!(*(str + i) ^ *(str + x)))
+			printf("Address of %c is: %p\n", *(str + i), str + i);
+		i++;
+	}
 }
 
 int main(void)
 {
 	/* TODO: Test functions */
-
+	char *str = (char *)malloc(max_length);
+	if (!str) {
+		printf("Malloc failed\n");
+		exit(-1);
+	}
+	scanf("%s", str);
+	printf("Lungimea sirului este %d\n", my_strlen(str));
+	equality_check(str);
+	free(str);
 	return 0;
 }
 
