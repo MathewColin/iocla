ca8e571c09602e834fd0fafb3682e680446ce7cf
Ceausu Matei Calin <mateicalin2002@yahoo.com>
2022-03-11 18:51:58 +0200

    Laborator 3, add solution for exercise 6.
    Signed-off-by: Ceausu Matei Calin <mateicalin2002@yahoo.com>

--- laborator/content/etapele-compilarii/06-obj-link-dev/main.c
+++ laborator/content/etapele-compilarii/06-obj-link-dev/main.c
@@ -1,3 +1,5 @@
+extern int price, qty;
+
 int main(void)
 {
 	/*
@@ -7,6 +9,9 @@ int main(void)
 	 * without directly calling a printing function from an existing
 	 * library(do NOT use printf, fprintf, fwrite, ...).
 	 */
-
+	set_price(21);
+	qty = 42;
+	print_price();
+	print_quantity();
 	return 0;
 }
