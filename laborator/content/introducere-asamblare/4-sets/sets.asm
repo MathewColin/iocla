75f6327f53b87c3bbf4eba98cef5e8e564781641
Ceausu Matei Calin <mateicalin2002@yahoo.com>
2022-04-07 14:40:15 +0300

    Add solution for lab5, exercise 4:
    Signed-off-by: Ceausu Matei Calin <mateicalin2002@yahoo.com>

--- laborator/content/introducere-asamblare/4-sets/sets.asm
+++ laborator/content/introducere-asamblare/4-sets/sets.asm
@@ -12,21 +12,50 @@ main:
     PRINTF32 `%u\n\x0`, ebx ; afiseaza cea de-a doua multime
 
     ; TODO1: reuniunea a doua multimi
-
+    xor ecx, ecx
+    mov ecx, eax
+    or ecx, ebx
+    PRINTF32 `Reuniunea este: \x0`
+    PRINTF32 `%u\n\x0`, ecx
 
     ; TODO2: adaugarea unui element in multime
-
+    xor ecx, ecx
+    mov ecx, 1
+    shl ecx, 2
+    or ecx, eax
+    PRINTF32 `Dupa adaugarea elementului 2, multimea este: \x0`
+    PRINTF32 `%u\n\x0`, ecx
 
     ; TODO3: intersectia a doua multimi
-
+    xor ecx, ecx
+    mov ecx, eax
+    and ecx, ebx
+    PRINTF32 `Intersectia este: \x0`
+    PRINTF32 `%u\n\x0`, ecx
 
     ; TODO4: complementul unei multimi
-
+    xor ecx, ecx
+    mov ecx, eax
+    not ecx
+    PRINTF32 `Complementul este: \x0`
+    PRINTF32 `%u\n\x0`, ecx
 
     ; TODO5: eliminarea unui element
-
+    xor ecx, ecx
+    mov ecx, 1
+    shl ecx, 3
+    not ecx
+    and ecx, eax
+    PRINTF32 `Multimea dupa eliminarea elementului 3 este: \x0`
+    PRINTF32 `%u\n\x0`, ecx
 
     ; TODO6: diferenta de multimi EAX-EBX
+    xor ecx, ecx
+    mov ecx, ebx
+    not ecx
+    and ecx, eax
+    PRINTF32 `Diferenta multimiilor este: \x0`
+    PRINTF32 `%u\n\x0`, ecx
 
 
     xor eax, eax
