4bc04d7544305abf6b2dd3ef34a72f1c03526a30
Ceausu Matei Calin <mateicalin2002@yahoo.com>
2022-04-09 13:25:10 +0300

    Add solution for Lab6, exercise 1 and 2:
    Signed-off-by: Ceausu Matei Calin <mateicalin2002@yahoo.com>

--- laborator/content/rolul-registrelor-adresare/1-2-multiply/multiply.asm
+++ laborator/content/rolul-registrelor-adresare/1-2-multiply/multiply.asm
@@ -22,7 +22,7 @@ main:
     mov al, byte [num1]
     mov bl, byte [num2]
     mul bl
-    
+
     ; Print result in hexa
     PRINTF32 `%s\x0`, print_mesaj
     xor ebx, ebx
@@ -31,6 +31,34 @@ main:
 
 
    ; TODO: Implement multiplication for dw and dd data types.
+
+   ; Multiplication for word
+    xor eax, eax
+    xor ebx, ebx
+    mov ax, word [num1_w]
+    mov bx, word [num2_w]
+    mul ebx
+
+    ; Print result in hexa
+    PRINTF32 `%s\x0`, print_mesaj
+    xor ebx, ebx
+    mov ebx, eax
+    PRINTF32 `%x\n\x0`, eax
+
+    ; Multiplication for dd
+    xor edx, edx
+    xor eax, eax
+    xor ebx, ebx
+    mov eax, dword [num1_d]
+    mov ebx, dword [num2_d]
+    mul ebx
+
+    ; Print result in hexa
+    PRINTF32 `%s\x0`, print_mesaj
+    xor ebx, ebx
+    mov ebx, eax
+    PRINTF32 `%x\x0`, edx
+    PRINTF32 `%x\n\x0`, eax
 
     leave
     ret
