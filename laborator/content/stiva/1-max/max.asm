a0d19a61f92c4285df80fae300bdd063617d175c
Ceausu Matei Calin <mateicalin2002@yahoo.com>
2022-05-01 19:49:05 +0300

    Laborator 8, exercitiul 1:
    Signed-off-by: Ceausu Matei Calin <mateicalin2002@yahoo.com>

--- laborator/content/stiva/1-max/max.asm
+++ laborator/content/stiva/1-max/max.asm
@@ -6,11 +6,18 @@ extern printf
 global main
 main:
     ; numbers are placed in these two registers
-    mov eax, 1
-    mov ebx, 4 
+    mov eax, 4
+    mov ebx, 1
 
     ; TODO: get maximum value. You are only allowed to use one conditional jump and push/pop instructions.
 
+    cmp ebx, eax
+    jl jump
+    push eax
+    push ebx
+    pop eax
+    pop ebx
+jump:
     PRINTF32 `Max value is: %d\n\x0`, eax ; print maximum value
-
+    xor eax, eax
     ret
