8ea18470b3c21b416427a935a4aa0738c83d3bf2
Ceausu Matei Calin <mateicalin2002@yahoo.com>
2022-05-01 19:49:50 +0300

    Laborator 8, exercitiul 2:
    Signed-off-by: Ceausu Matei Calin <mateicalin2002@yahoo.com>

--- laborator/content/stiva/2-reverse/reverse-array.asm
+++ laborator/content/stiva/2-reverse/reverse-array.asm
@@ -14,8 +14,28 @@ global main
 main:
 
     ; TODO push the elements of the array on the stack
+    mov ecx, ARRAY_LEN
+    sub ecx, 1
+label:
+    mov eax, dword[input + 4 * ecx]
+    push eax
+    test ecx, ecx
+    jz pr
+    sub ecx, 1
+    jmp label
     ; TODO retrieve the elements (pop) from the stack into the output array
-
+pr:
+    mov ecx, ARRAY_LEN
+    xor edx, edx
+    sub ecx, 1
+iesire:
+    pop eax
+    mov [output + 4 * ecx], eax
+    test ecx, ecx
+    jz print
+    sub ecx, 1
+    jmp iesire
+print:
     PRINTF32 `Reversed array: \n\x0`
     xor ecx, ecx
 print_array:
