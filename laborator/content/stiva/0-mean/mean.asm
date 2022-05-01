1ea1922ec8111ac9e2d083e7c5285052b4d91ff7
Ceausu Matei Calin <mateicalin2002@yahoo.com>
2022-05-01 19:48:04 +0300

    Laborator 8, execitiul 0:
    Signed-off-by: Ceausu Matei Calin <mateicalin2002@yahoo.com>

--- laborator/content/stiva/0-mean/mean.asm
+++ laborator/content/stiva/0-mean/mean.asm
@@ -19,23 +19,41 @@ main:
 
     ; TODO1 - compute the sum of the vector numbers - store it in eax
 
+    sub ecx, 1
+suma:
+    add ax, word[num_array + 2 * ecx]
+    test ecx, ecx
+    jz pr
+    sub ecx, 1
+    jmp suma
+
+pr:
     PRINTF32 `Sum of numbers: %d\n\x0`, eax
 
     ; TODO2 - compute the quotient of the mean
 
-    PRINTF32 `Mean of numbers: %d\x0`, eax
-    PRINTF32 `.\x0`
+    mov ecx, eax
+    mov ax, cx
+    shr ecx, 16
+    mov dx, cx
+    mov bx, ARRAY_SIZE
+    div bx
+
+    PRINTF32 `Mean of numbers: %d\n\x0`, eax
+    xor eax, eax
+    ret
+    ;PRINTF32 `.\x0`
 
     mov ecx, DECIMAL_PLACES
 compute_decimal_place:
 
     ; TODO3 - compute the current decimal place - store it in ax
 
-    PRINTF32 `%d\x0`, eax
-    dec ecx
-    cmp ecx, 0
-    jg compute_decimal_place
+    ;PRINTF32 `%d\x0`, eax
+    ;dec ecx
+    ;cmp ecx, 0
+    ;jg compute_decimal_place
 
-    PRINTF32 `\n\x0`
-    xor eax, eax
+    ;PRINTF32 `\n\x0`
+    ;xor eax, eax
     ret
